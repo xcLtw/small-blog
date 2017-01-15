d4f92d8c94d245487f8583bf44049fe75f60932f
chang xu <xcltw1993@gmail.com>
2017-01-15 18:41:59 +0800

    config file update

---
+++ config.py
@@ -0,0 +1,46 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess hehe'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    FLASK_MAIL_SUBJECT_PREFIX = '[FLASK-Xc]'
+    FLASK_MAIL_SENDER = 'Flask Admin <xcltw1993@gmail.com>'
+    FLASK_ADMIN = os.environ.get('FLASK_ADMIN')
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    MAIL_SERVER = 'smpt.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URI') or \
+                              'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URI') or \
+                              'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URI') or \
+                              'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+
+    'default': DevelopmentConfig
+}
