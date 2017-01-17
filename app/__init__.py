9d4ac021ce9e0b3aaf0a4703e14328e03156ee28
xuchang <chang.xu@speedx.com>
2017-01-17 17:14:37 +0800

    p89,注册新用户

--- app/__init__.py
+++ app/__init__.py
@@ -25,6 +25,7 @@ def create_app(config_name):
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     # blueprint
 
--- app/auth/views.py
+++ app/auth/views.py
@@ -3,7 +3,8 @@ from flask import render_template, redirect, request, url_for, flash
 from flask_login import login_user, logout_user, login_required
 from app.auth import auth
 from app.models import User
-from app.auth.forms import LoginForm
+from app import db
+from app.auth.forms import LoginForm, RegistrationForm
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -24,3 +25,19 @@ def logout():
     logout_user()
     flash('You have been logged out')
     return redirect(url_for('main.index'))
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(
+            email=form.email.data,
+            username=form.username.data,
+            password=form.password.data
+        )
+        db.session.add(user)
+        db.session.commit()
+        flash('You can now login.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
